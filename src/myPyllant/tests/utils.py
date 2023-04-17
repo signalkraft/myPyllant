ab325855719229cd32c4212ea0795cc5aae16854
Philipp Dörner <pd@signalkraft.com>
2023-04-17 17:55:45 +0200

    Added more (dummy) responses to device buckets test data, fixed expires_in on mocked API

--- src/myPyllant/tests/utils.py
+++ src/myPyllant/tests/utils.py
@@ -124,5 +124,7 @@ async def _mocked_api(*args, **kwargs) -> MyPyllantAPI:
     api.oauth_session = {
         "access_token": "access_token",
         "refresh_token": "refresh_token",
+        "expires_in": 3600,
     }
+    api.set_session_expires()
     return api
